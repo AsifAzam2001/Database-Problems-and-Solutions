120f1ff49f1f86dafe46e24a4f1a1e41f9e2512a
Asif Azam <96988265+AsifAzam2001@users.noreply.github.com>
2025-09-05 20:53:05 +0600

    Time: 608 ms (46.10%) | Memory: 0B (100.00%) - LeetSync

---
+++ leetcode-sql-50/1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -0,0 +1,38 @@
+WITH
+    transCTE AS (
+        SELECT
+            DATE_FORMAT(trans_date, '%Y-%m') AS month,
+            country,
+            COUNT(id) AS trans_count,
+            SUM(amount) AS trans_total_amount
+        FROM
+            Transactions
+        GROUP BY
+            month,
+            country
+    ),
+    approvedCTE AS (
+        SELECT
+            DATE_FORMAT(trans_date, '%Y-%m') AS month,
+            country,
+            COUNT(id) AS approved_count,
+            SUM(amount) AS approved_total_amount
+        FROM
+            Transactions
+        WHERE
+            state = 'approved'
+        GROUP BY
+            month,
+            country
+    )
+
+SELECT
+    c.month,
+    c.country,
+    c.trans_count,
+    COALESCE(a.approved_count, 0) AS approved_count,
+    c.trans_total_amount,
+    COALESCE(a.approved_total_amount, 0) AS approved_total_amount
+FROM
+    transCTE c
+    LEFT JOIN approvedCTE a ON c.month = a.month AND c.country <=> a.country;
