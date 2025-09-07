60f4f21b0b45b4de5802bf8bda5b8a0c7782ae3e
Asif Azam <96988265+AsifAzam2001@users.noreply.github.com>
2025-09-07 21:43:30 +0600

    Time: 726 ms (28.22%) | Memory: 0B (100.00%) - LeetSync

--- leetcode-sql-50/1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ leetcode-sql-50/1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,4 +1,29 @@
 WITH
+    RankedOrders AS (
+        SELECT
+            order_date,
+            customer_pref_delivery_date,
+            RANK() OVER (
+                PARTITION BY customer_id
+                ORDER BY order_date ASC
+            ) as order_rank
+        FROM
+            Delivery
+    )
+SELECT
+    ROUND(
+        AVG(IF(order_date = customer_pref_delivery_date, 1, 0)) * 100,
+        2
+    ) AS immediate_percentage
+FROM
+    RankedOrders
+WHERE
+    order_rank = 1;
+
+/*
+# Alternative Solution:
+
+WITH
     firstOrderCTE AS (
         SELECT
             customer_id,
@@ -19,3 +44,5 @@ FROM
     Delivery d
     JOIN firstOrderCTE f ON d.customer_id = f.customer_id
     AND d.order_date = f.first_order;
+
+*/
