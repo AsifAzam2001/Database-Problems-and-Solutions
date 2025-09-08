b9532c212a70e9231b4dc27d6932a1434992b24f
Asif Azam <96988265+AsifAzam2001@users.noreply.github.com>
2025-09-08 18:30:04 +0600

    Time: 675 ms (83.01%) | Memory: 0B (100.00%) - LeetSync

---
+++ leetcode-sql-50/1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -0,0 +1,17 @@
+WITH
+    RankedSales AS (
+        SELECT
+            *,
+            RANK() OVER (
+                PARTITION BY product_id
+                ORDER BY year ASC
+            ) as year_rank
+        FROM
+            Sales
+    )
+SELECT
+    product_id, year AS first_year, quantity, price
+FROM
+    RankedSales
+WHERE
+    year_rank = 1;
